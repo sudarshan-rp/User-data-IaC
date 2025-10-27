178ca481272fc2cd5127b94b58d109bf07e7bc94
boss0077 <sudarshanrpgowda7@gmail.com>
2025-10-27 21:42:57 +0530

    assign public IP

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -17,7 +17,8 @@ resource "aws_subnet" "public" {
     count = length(var.public_subnet_cidrs)
     vpc_id            = aws_vpc.custom.id
     cidr_block        = var.public_subnet_cidrs[count.index]
-    availability_zone = var.availability_zones[count.index] 
+    availability_zone = var.availability_zones[count.index]
+    map_public_ip_on_launch = true 
 }
 
 resource "aws_internet_gateway" "igw" {
