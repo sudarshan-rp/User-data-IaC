5a25f313a92bc6c20f3887e05e35283148164f10
boss0077 <sudarshanrpgowda7@gmail.com>
2025-10-28 15:31:28 +0530

    remove public IP

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -18,7 +18,7 @@ resource "aws_subnet" "public" {
     vpc_id            = aws_vpc.custom.id
     cidr_block        = var.public_subnet_cidrs[count.index]
     availability_zone = var.availability_zones[count.index]
-    map_public_ip_on_launch = true 
+    #map_public_ip_on_launch = true 
 }
 
 resource "aws_internet_gateway" "igw" {
